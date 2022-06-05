cef5da571c45faf02a64558d35fd1d09cf4a0b77
mattun0819 <syunsuke1018@icloud.com>
2022-06-05 17:01:26 +0900

    テスト

---
+++ blinkled/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define LED_PIN 15
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+  Serial.printf("Program Start\n");
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED_PIN, HIGH);
+  Serial.printf("LED_PIN - HIGH\n");
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  Serial.printf("LED_PIN - LOW\n");
+  delay(1000);
+}
